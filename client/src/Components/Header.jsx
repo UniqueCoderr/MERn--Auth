a1ec6d2a03c554993e2f8d6682f53e40dbb9e3a8
Touseef Abbas <touseef.2167@gmail.com>
2024-06-29 12:37:51 +0500

    Update Header Component and Complete it

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 export default function Header() {
   return (
    <header className='bg-slate-200'>
     <div className="flex justify-between max-w-7xl mx-auto items-center p-3">
       <div className="font-bold">
-        <h1>Auth App</h1>
+      <Link to = '/' ><h1>Auth App</h1> </Link>
       </div>
       <div className="">
         <ul className='flex space-x-10'>
-          <li>Home</li>
-          <li>About</li>
-          <li>SignIn</li>
+          <Link to = '/' ><li className='cursor-pointer ' >Home</li></Link>
+          <Link to = '/about' ><li>About</li></Link>
+          <Link to = '/sign-in' ><li>SignIn</li></Link>
         </ul>
       </div>
     </div>
