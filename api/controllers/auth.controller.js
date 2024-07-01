3066730b82d31b565e4d2b20cfd4e550d2bce48e
Touseef Abbas <touseef.2167@gmail.com>
2024-07-01 16:06:13 +0500

    create sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
+// import { createError } from "../utils/error.js";
 export const signup = async(req, res, next) => {
     const { username, email, password } = req.body;
     const hashedPassword =  bcryptjs.hashSync(password, 10)
@@ -12,4 +14,26 @@ export const signup = async(req, res, next) => {
         next(error)
     }
    
+}
+
+export const signin = async(req, res, next) => {
+    const { email, password } = req.body;
+    try {
+        const validUser = await User.findOne({email});
+        if (!validUser) {    
+            return next(error)
+        }
+        const isPasswordCorrect =  bcryptjs.compareSync(password, validUser.password);
+        if (!isPasswordCorrect) {
+            return next(error)
+        }
+        const token = jwt.sign({id: validUser._id, email: validUser.email}, process.env.JWT_SECRET);
+        const {password: hashedPassword, ...rest } = validUser._doc
+        const expirationDate = new Date(Date.now()+ 3600000); // 1 hour from now
+        res.cookie("access_token", token, {httpOnly: true , expires: expirationDate}).status(200).json(rest)
+        
+        
+    } catch (error) {
+        next(error)
+    }
 }
